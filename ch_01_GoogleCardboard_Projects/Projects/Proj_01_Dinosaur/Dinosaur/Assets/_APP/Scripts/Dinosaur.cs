86ec20694b3ca2f621484f3a48a0ef1992ebcec7
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-04-27 21:22:22 +0200

    add ch_01_GoogleCardboard_Projects

---
+++ ch_01_GoogleCardboard_Projects/Projects/Proj_01_Dinosaur/Dinosaur/Assets/_APP/Scripts/Dinosaur.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dinosaur : MonoBehaviour {
+
+    public GameObject din;
+
+    public void ZoomIn()
+    {
+        din.transform.localScale += new Vector3(1F, 1F, 1F);
+    }
+}
